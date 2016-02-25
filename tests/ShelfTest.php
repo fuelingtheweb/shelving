50b85af4b89ff04c8847e693e2852c2c3554a4c9
Nathan Morgan <nathan@fuelingtheweb.com>
2016-02-25 04:48:58 -0500

    Add test for getting model and fix issue with calling protected methods from tests improperly

--- tests/ShelfTest.php
+++ tests/ShelfTest.php
@@ -8,10 +8,43 @@ class ShelfTest extends PHPUnit_Framework_TestCase
     private $shelf;
 
     /**
+     * Model class name to be used
+     * for setting model property of $shelf
+     * and mocking the model class
+     */
+    private $modelClass = 'SomeClass';
+
+    /**
      * Set $shelf property to mock for the abstract Shelf class
+     * Add model property
+     * Mock the model class
      */
     public function setUp() {
         $this->shelf = $this->getMockForAbstractClass('Fuelingtheweb\Shelving\Shelf');
+        $this->setProperty('model', $this->modelClass);
+        $this->getMockBuilder($this->modelClass)->getMock();
+    }
+
+    /**
+     * Set property of mocked Shelf
+     */
+    private function setProperty($name, $value) {
+        $r = new ReflectionClass(get_class($this->shelf));
+        $prop = $r->getProperty($name);
+        $prop->setAccessible(true);
+        $prop->setValue($this->shelf, $value);
+        $prop->setAccessible(false);
+    }
+
+    /**
+     * Invoke protected or private method on $shelf
+     */
+    private function invokeMethod($name, $args = []) {
+        $r = new ReflectionClass(get_class($this->shelf));
+        $method = $r->getMethod($name);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($this->shelf, $args);
     }
 
     /**
@@ -22,6 +55,16 @@ class ShelfTest extends PHPUnit_Framework_TestCase
      * @expectedExceptionMessage The model property must be a string name of your model class.
      */
     public function testExceptionWhenModelPropIsNull() {
-        $this->shelf->getModelClass();
+        $this->setProperty('model', null);
+        $this->invokeMethod('getModelClass');
+    }
+
+    /**
+     * A new instance of the model class name should be returned
+     */
+    public function testGetModelReturnsNewClass() {
+        $model = $this->invokeMethod('getModel');
+
+        $this->assertInstanceOf($this->modelClass, $model);
     }
 }
